20fc544266e9434ca1b933dd0b7bebbc23bffac1
Sri Yalamanchili <sri.yalamanchili@contractor.isbanuk.com>
2019-03-06 11:30:06 +0000

    new files

---
+++ src/main/java/com/example/msspringsecurityapp/DemoApplication.java
@@ -0,0 +1,30 @@
+package com.example.msspringsecurityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("resource")
+	@ResponseBody
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World !!");
+		return model;
+	}
+
+}
